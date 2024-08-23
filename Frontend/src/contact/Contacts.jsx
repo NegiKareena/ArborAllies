edf0a389de92b9822befc94db7aa7d0828662fa9
Kareena Negi <159912897+NegiKareena@users.noreply.github.com>
2024-08-24 00:01:36 +0530

    Add files via upload

---
+++ Frontend/src/contact/Contacts.jsx
@@ -0,0 +1,18 @@
+
+import Navbar from '../components/Navbar';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+
+function Contacts() {
+  return (
+    <>
+      <Navbar />
+      <div className='min-h-screen'>
+        <Contact />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Contacts;
